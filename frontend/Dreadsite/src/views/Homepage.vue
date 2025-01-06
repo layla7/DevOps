1de94aad8289b268c2b5d3f93d48dbdb5c9510e5
layla7 <laylamouat04@gmail.com>
2025-01-06 16:57:13 +0000

    Test

--- frontend/Dreadsite/src/views/Homepage.vue
+++ frontend/Dreadsite/src/views/Homepage.vue
@@ -4,7 +4,7 @@ import VideoCard from "../components/VideoCard.vue";
 
 <template>
   <div>
-    <h1>Test</h1>
+    <h1>Hi!!!!!</h1>
     <div class="d-flex justify-content" style = " overflow: auto; white-space: nowrap;">
       <VideoCard v-for="n in 10" />
     </div>
