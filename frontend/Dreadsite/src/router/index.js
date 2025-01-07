6e9a2c63a6a5eb779ba8b08a840c1fbdb654bc36
layla7 <laylamouat04@gmail.com>
2025-01-07 17:57:49 +0000

    Blueprint videopage

--- frontend/Dreadsite/src/router/index.js
+++ frontend/Dreadsite/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '../views/Homepage.vue'
 import LoginPage from '../views/LoginPage.vue'
+import Videopage from '../views/Videopage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginPage
+    },
+    {
+      path: '/video',
+      name: 'video',
+      component: Videopage
     }
   ],
 })
---
+++ frontend/Dreadsite/src/views/Videopage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="ratio ratio-16x9 mb-4">
+                    <video controls>
+                        <source src="https://d3ocjjcdyw5d62.cloudfront.net/Spectrum.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <h2 class="mb-3">Video Title</h2>
+                <p class="text-muted">This is the description of the video. Here you can provide additional details about the content, context, or any other relevant information for the viewers.</p>
+            </div>
+        </div>
+    </div>
+</template>
