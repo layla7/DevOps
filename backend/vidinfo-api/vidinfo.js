2ae5bfdf519f71981951f20645912be1255d9f34
layla7 <laylamouat04@gmail.com>
2025-01-08 15:59:33 +0000

    vidinfo initial

--- backend/vidinfo-api/vidinfo.js
+++ backend/vidinfo-api/vidinfo.js
@@ -1,9 +1,12 @@
-const dotenv = require("dotenv");
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import dotenv from "dotenv";
+import express from "express";
+
+const client = new DynamoDBClient({ region : "us-east-1" });
+
+
 dotenv.config();
 
-//console.log(process.env);
-
-const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
@@ -12,6 +15,17 @@ const port = process.env.API_PORT || 3000;
 
 app.get("/test", async (req, res) => {
     res.json({"message" : "Successful"});
+})
+
+app.get("/videos", async(req, res) => {
+    const params = {
+        TableName : "Videos",
+    }
+
+    const scanCommand = new ScanCommand(params);
+    const response = await client.send(scanCommand).Item;
+
+    res.send(response);
 })
 
 
