e177d0bc40b00f4568377f7a25c114f8c061c6a5
layla7 <laylamouat04@gmail.com>
2025-01-12 23:36:51 +0000

    commit for demo

--- backend/lambda/user_login/index.mjs
+++ backend/lambda/user_login/index.mjs
@@ -1,7 +1,7 @@
 import { DynamoDBClient, GetItemCommand} from "@aws-sdk/client-dynamodb";
 import crypto from "crypto";
 
-//
+//Lowrie woz here!
 
 const client = new DynamoDBClient({ region: "us-east-1" });
 
